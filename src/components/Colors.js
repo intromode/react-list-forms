77e30cd868ce66edd2cc015e6da9ca4fa608ba89
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-05-15 13:56:06 -0700

    switched to key=i

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -5,10 +5,10 @@ import Color from './Color';
 //have to destructor otherwise you have to do colors.colors. so it was store the array as an object named colors. guess it passes over as an object? 
 export default function Colors({ colors }) {
   //dont have to give Color hex, with color.hex, because the Color component gets the hex from the rgb
-  const colorList = colors.map(color => {
+  const colorList = colors.map((color, i) => {
     return (
       <>
-      <li key={color.name}>
+      <li key={i}>
         <Color name={color.name} rgb={color.rgb} />
       </li>
       </>
@@ -17,7 +17,6 @@ export default function Colors({ colors }) {
 
   return (
     <ul>
-      {/* dont need the fragment wrapper? */}
       <>
       {colorList}
       </>
